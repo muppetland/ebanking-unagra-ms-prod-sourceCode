6d1338045b6b609197bfe8237e1c7cc29141c4ae
Ivan Hazael Botello Fermoso <IvanB@sfa.loc>
2023-09-13 12:22:26 -0600

    edited some files 'cause not were available to communicate between each microservice in docker.

--- ebanking-api/src/main/java/com/unagra/ebankingapi/dto/DetailBankInstitutionDTO.java
+++ ebanking-api/src/main/java/com/unagra/ebankingapi/dto/DetailBankInstitutionDTO.java
@@ -12,16 +12,8 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 public class DetailBankInstitution {
-    @Id
-    @Column(name = "keymatch", nullable = false)
     private String keymatch;
-
-    @Column(name = "cvespei", nullable = false)
     private Integer cvespei;
-
-    @Column(name = "institution", nullable = false)
     private String institution;
-
-    @Column(name = "iscard", nullable = false)
     private Integer iscard;
 }
